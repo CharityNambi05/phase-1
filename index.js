681903ed6f3ff7a0a9241c9aeb5031f9ffb68515
CharityNambi05 <charity.nambi@icloud.com>
2024-07-01 23:57:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function calculateGrade() {
+    const marks = parseInt(document.getElementById(" marks").value);
+    let grade;
+    if (marks > 79){
+        grade = "A";
+    }else if (marks >= 60 && marks <= 79){
+        grade = "B";
+    }else if (marks >= 50 && marks <= 59){
+        grade = "C";
+    }else if (marks >=40 && marks <= 49){
+        grade = "D";
+    }else {
+        grade = "E";
+    }
+    document.getElementById("grade").textContent=`Grade: ${grade}`;
+}
